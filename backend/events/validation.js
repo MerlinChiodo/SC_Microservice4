515250ff9b17cf5ea99051a3a5e1ec04b8a186e9
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-05-15 14:59:47 +0200

    created Schema for the event 'about us' for the LandingPage

--- backend/events/validation.js
+++ backend/events/validation.js
@@ -3,9 +3,11 @@ const Ajv = require('ajv');
 
 const schema_event_Calendar = require('event_Calendar.json');
 const schema_event_NoCalendar = require('event_NoCalendar.json');
+const schema_aboutUs = require("./landingPage/event_aboutUs.json")
 
 
 const ajv = exports.ajv = new Ajv();
 
 ajv.addSchema(schema_event_Calendar, 'event_Calendar');
 ajv.addSchema(schema_event_NoCalendar, 'event_NoCalendar');
+ajv.addSchema(schema_aboutUs, 'aboutUs')
