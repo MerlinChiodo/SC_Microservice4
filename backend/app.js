4adb65dae3b9cceb475c34ad906688d372457476
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-05-23 11:47:41 +0200

    installed cors; sending an event to rabbitmq via frontend works now

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require(`cors`);
 
 
 const indexRouter = require('./routes/index');
@@ -21,6 +22,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
