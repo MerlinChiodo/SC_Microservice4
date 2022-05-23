761693777a14f68ce811bc34dc9dae9506cb9e63
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-05-23 14:23:00 +0200

    added correct backendurl

--- frontend/src/controllers/aboutUsController.js
+++ frontend/src/controllers/aboutUsController.js
@@ -1,6 +1,6 @@
-const backendurl = "http://localhost:3001/eventTest/";
+//const backendurl = "http://localhost:3001/eventTest/";
 
-//const backendurl = "http://vps2290194.fastwebserver.de:9740/eventTest/";
+const backendurl = "http://vps2290194.fastwebserver.de:9740/eventTest/";
 
 export async function testAboutUs() {
     const requestOptions = {
