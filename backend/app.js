9116976771baccf16e2bbd7d4f053939e1880896
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-05-26 15:23:27 +0200

    commented out corse

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(cors({
+/*app.use(cors({
   origin: 'http://localhost:3000'
-}))
+}))*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
