e52148b8737dac4b4bf6f4c2b637babb3b761c10
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-06-06 15:28:28 +0200

    deleted not used attributes and the list version of dataView

--- frontend/src/components/postCard.vue
+++ frontend/src/components/postCard.vue
@@ -2,32 +2,9 @@
   <div class="card">
     <DataView :value="posts" :layout="layout" :paginator="true" :rows="9" >
       <template #header>
-        <div class="grid grid-nogutter">
-          <div class="col-6" style="text-align: left">
-          </div>
-          <div class="col-6" style="text-align: right">
-            <DataViewLayoutOptions v-model="layout" />
-          </div>
-        </div>
+
       </template>
 
-      <template #list="slotProps">
-        <div class="col-12">
-          <div class="product-list-item">
-            <!--<img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" :alt="slotProps.data.title"/>-->
-            <div class="product-list-detail">
-              <span class="post-service">Serivce{{slotProps.data.service}}</span>
-              <div class="post-title">Titel: {{slotProps.data.title}}</div>
-              <div class="post-short_description">kurze Beschreibung: {{slotProps.data.short_description}}</div>
-
-            </div>
-            <div class="product-list-action">
-              <Button label="Details" :disabled="slotProps.data.long_description === ''"></Button>
-              <!--<span :class="'product-badge status-'+slotProps.data.inventoryStatus.toLowerCase()">{{slotProps.data.inventoryStatus}}</span>-->
-            </div>
-          </div>
-        </div>
-      </template>
 
       <template #grid="slotProps">
         <div class="col-12 md:col-4">
@@ -62,13 +39,7 @@ export default {
     return {
       posts: null,
       layout: 'grid',
-      sortKey: null,
-      sortOrder: null,
-      sortField: null,
-      sortOptions: [
-        {label: 'Price High to Low', value: '!created_on'},
-        {label: 'Price Low to High', value: 'created_on'},
-      ]
+      
     }
   },
 
