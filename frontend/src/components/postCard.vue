83d7de3342e098d638b62fcede46b88712d0588d
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-06-06 09:57:51 +0200

    defining the function in postCard fixed the problem of having promises instead of array; successfully got data from backend to frontend and put it in webpage;

--- frontend/src/components/postCard.vue
+++ frontend/src/components/postCard.vue
@@ -6,9 +6,9 @@
         <div>
           <img :src="'demo/images/car/' + slotProps.data.title + '.png'" :alt="slotProps.data.title"/>
           <div class="grid">
-            <div class="col-12">Vin: <b>{{slotProps.data.title}}</b></div>
-            <div class="col-12">Year: <b>{{slotProps.data.service}}</b></div>
-            <div class="col-12">Brand: <b>{{slotProps.data.short_description}}</b></div>
+            <div class="col-12">Titel: <b>{{slotProps.data.title}}</b></div>
+            <div class="col-12">Service: <b>{{slotProps.data.service}}</b></div>
+            <div class="col-12">Kurzbeschreibung <b>{{slotProps.data.short_description}}</b></div>
           </div>
         </div>
         <Button icon="pi pi-search"></Button>
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+const backendurl = "http://localhost:3001/";
 import {getAllServicePosts} from '../controllers/postController'
 export default {
   name: "postCard",
@@ -30,10 +31,24 @@ export default {
   },
   postService: null,
 
-  mounted() {
-    this.posts = getAllServicePosts()
+  mounted: function(){
+    this.getServicePosts();
+
+    //this.posts = getAllServicePosts()
+
   },
   methods: {
+    getServicePosts(){
+      const options = {
+        method: 'GET'
+      };
+      fetch(backendurl + "posts/getAllServicePosts", options)
+          .then((response) => response.json())
+          .then((data) => {
+            this.posts = data
+          })
+          .catch(error => {console.log(error)});
+    }
     }
 
 }
