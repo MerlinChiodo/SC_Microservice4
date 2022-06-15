8df8a9ee5853f13a3c28729b7f0f2938e529099c
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-06-15 14:47:08 +0200

    trying to pass props to childcomponent (UserPostView to UserPostCard; not working yet

--- frontend/src/components/UserPostCard.vue
+++ frontend/src/components/UserPostCard.vue
@@ -1,32 +1,21 @@
 <template>
   <div>
-    <Card style="width: 25rem; margin-bottom: 2em">
-      <template #title>
-        Simple Card
-      </template>
-      <template #content>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-          quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
-      </template>
-    </Card>
-
     <Card style="width: 25em">
       <template #header>
         <img src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png" style="height: 15rem" />
       </template>
-      <template #title>
-        Advanced Card
+      <template v-if="post" #title>
+        {{post.title || "not yet"}}
       </template>
       <template #subtitle>
         Card subtitle
       </template>
-      <template #content>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-          quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+      <template v-if="post" #content>
+        <p>{{post.short_description|| "not yet"}}</p>
       </template>
       <template #footer>
-        <Button icon="pi pi-check" label="Save" />
-        <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
+        <Button label="Details" />
+
       </template>
     </Card>
   </div>
@@ -34,7 +23,11 @@
 
 <script>
 export default {
-  name: "UserPostCard"
+  name: "UserPostCard",
+  props:{
+    post: Object
+
+  }
 }
 </script>
 
--- frontend/src/views/UserPostView.vue
+++ frontend/src/views/UserPostView.vue
@@ -1,5 +1,5 @@
 <template>
-<user-post-card></user-post-card>
+<user-post-card v-bind="post" ></user-post-card>
 </template>
 
 <script>
@@ -7,7 +7,15 @@ import UserPostCard from '../components/UserPostCard.vue'
 
 export default {
   name: "UserPostView",
-  components: {UserPostCard}
+  components: {UserPostCard},
+  data() {
+    return{
+      post: {
+        title: "Dies ist ein titel",
+        short_description: "kurze Beschreibung"
+      }
+    }
+  }
 }
 </script>
 
