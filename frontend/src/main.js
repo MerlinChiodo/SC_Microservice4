b93184813391f12b40c5fc79003271d503f75598
mbraun6 <93530350+mbraun6@users.noreply.github.com>
2022-06-27 17:06:56 +0200

    installed and imported vue cookies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
+import VueCookies from "vue-cookies";
 
 //import "primevue/resources/themes/md-light-deeppurple/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -30,6 +31,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(VueCookies);
+
 
 
 app.provide("backendurl", "http://localhost:3001/");
